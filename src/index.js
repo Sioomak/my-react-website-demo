dc445c660305746167d9a698dbdf0d315dd99259
Siamak Salimi <ssalimi1220@gmail.com>
2020-10-17 17:44:41 -0400

    first commit: public folder is ready

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+
+function Greeting(){
+  return <h4>My first component! Salam Buddies</h4>;
+}
+
+
+ReactDOM.render(<App/>, document.getElementById('root'));
+
+
