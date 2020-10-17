e3f5c674afef32be0a469371120ff53e2ef47c54
Siamak Salimi <ssalimi1220@gmail.com>
2020-10-17 19:11:19 -0400

    ButtonJS and ButtonCSS added

--- src/index.js
+++ src/index.js
@@ -3,11 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 
-function Greeting(){
-  return <h4>My first component! Salam Buddies</h4>;
-}
-
-
 ReactDOM.render(<App/>, document.getElementById('root'));
 
 
