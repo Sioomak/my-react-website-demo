d440102f8fb332a9b38a0976cc125d269e157331
Siamak Salimi <ssalimi1220@gmail.com>
2020-10-21 20:46:03 -0400

    Home components added

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,22 +1,34 @@
 import React from 'react'
+import '../App.css'
 import { Button } from './Button'
+import './HeroSection.css'
 
-function Herosection() {
+function HeroSection() {
   return (
-    <div className="hero-container" >
-      <video src="/video/video-2.mp4" autoPlay loop muted />
+    <div className='hero-container'>
+      <video src='/videos/video-2.mp4' autoPlay loop muted />
       <h1>ADVENTURE AWAITS</h1>
       <p>What are you waiting for?</p>
-      <div className="hero-btn" >
-        <Button className="btns" 
-        buttonStyle="btn--outline"
-        buttonSize="btn--large"
+      <div className='hero-btns'>
+        <Button
+          className='btns'
+          buttonStyle='btn--outline'
+          buttonSize='btn--large'
         >
-        GET STARTED
+          GET STARTED
+        </Button>
+        <Button
+          className='btns'
+          buttonStyle='btn--primary'
+          buttonSize='btn--large'
+          onClick={console.log('hey')}
+        >
+          WATCH TRAILER <i className='far fa-play-circle' />
         </Button>
       </div>
     </div>
-  )
+  );
 }
 
-export default Herosection
+
+export default HeroSection
