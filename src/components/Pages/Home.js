d691cb425941d79e94691be5a15f6dacc870c50b
Siamak Salimi <ssalimi1220@gmail.com>
2020-10-21 21:04:55 -0400

    CardItems and Card component added

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -1,14 +1,16 @@
-import React from 'react'
-import '../../App.css'
-import HeroSection from '../HeroSection'
 
-function Home(){
+import React from 'react';
+import '../../App.css';
+import Cards from '../Cards';
+import HeroSection from '../HeroSection';
+
+function Home() {
   return (
     <>
-    <HeroSection/>
+      <HeroSection />
+      <Cards />
     </>
   );
 }
-
 
 export default Home;
